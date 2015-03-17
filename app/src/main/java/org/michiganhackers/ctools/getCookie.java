40b10db844d6a988e9294b68d2352365df83c589
NickLamping <nlamping@umich.edu>
2015-03-16 21:19:42 -0400

    getting the cookie

---
+++ app/src/main/java/org/michiganhackers/ctools/getCookie.java
@@ -0,0 +1,61 @@
+package org.michiganhackers.ctools;
+
+import android.nfc.Tag;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+
+import java.net.CookieHandler;
+import java.net.CookieStore;
+
+
+public class getCookie extends ActionBarActivity {
+    public static final String TAG = "getCookie";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_cookie);
+
+        WebView webview = (WebView)findViewById(R.id.webview);
+
+        webview.loadUrl("https://weblogin.umich.edu/?cosign-ctools&https://ctools.umich.edu/sakai-login-tool/container");
+        onPageFinished(webview, "https://weblogin.umich.edu/?cosign-ctools&https://ctools.umich.edu/sakai-login-tool/container");
+        finish();
+    }
+
+
+    public void onPageFinished(WebView view, String url){
+        String cookies = CookieManager.getInstance().getCookie(url);
+
+        Log.d(TAG,"All the cookies in a string: " + cookies);
+        String[] cookiesList = cookies.split("cosign=");
+        String loginCookie = cookiesList[0];
+        CookieManager.getInstance().setCookie(url, loginCookie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_get_cookie, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
